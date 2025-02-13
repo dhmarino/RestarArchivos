5cd0ad0c89c1683b17db0a11d9f2706e58f1c31f
dhmarino <diego.marino@valid.com>
2025-02-13 09:11:51 -0300

    Commit Inicial

---
+++ RestarArchivos/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace RestarArchivos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BtnArchivoOriginal = new System.Windows.Forms.Button();
+            this.BtnArchivoRestar = new System.Windows.Forms.Button();
+            this.BtnSaveResult = new System.Windows.Forms.Button();
+            this.lblArchivoOriginal = new System.Windows.Forms.Label();
+            this.lblArchivoDescontar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnArchivoOriginal
+            // 
+            this.BtnArchivoOriginal.Location = new System.Drawing.Point(217, 70);
+            this.BtnArchivoOriginal.Name = "BtnArchivoOriginal";
+            this.BtnArchivoOriginal.Size = new System.Drawing.Size(356, 60);
+            this.BtnArchivoOriginal.TabIndex = 0;
+            this.BtnArchivoOriginal.Text = "Seleccionar Archivo Original";
+            this.BtnArchivoOriginal.UseVisualStyleBackColor = true;
+            this.BtnArchivoOriginal.Click += new System.EventHandler(this.BtnArchivoOriginal_Click);
+            // 
+            // BtnArchivoRestar
+            // 
+            this.BtnArchivoRestar.Location = new System.Drawing.Point(217, 222);
+            this.BtnArchivoRestar.Name = "BtnArchivoRestar";
+            this.BtnArchivoRestar.Size = new System.Drawing.Size(356, 60);
+            this.BtnArchivoRestar.TabIndex = 1;
+            this.BtnArchivoRestar.Text = "Selecciona archivo a descontar";
+            this.BtnArchivoRestar.UseVisualStyleBackColor = true;
+            this.BtnArchivoRestar.Click += new System.EventHandler(this.BtnArchivoRestar_Click);
+            // 
+            // BtnSaveResult
+            // 
+            this.BtnSaveResult.Location = new System.Drawing.Point(217, 391);
+            this.BtnSaveResult.Name = "BtnSaveResult";
+            this.BtnSaveResult.Size = new System.Drawing.Size(356, 60);
+            this.BtnSaveResult.TabIndex = 2;
+            this.BtnSaveResult.Text = "Guardar archivo resultante";
+            this.BtnSaveResult.UseVisualStyleBackColor = true;
+            this.BtnSaveResult.Click += new System.EventHandler(this.BtnSaveResult_Click_1);
+            // 
+            // lblArchivoOriginal
+            // 
+            this.lblArchivoOriginal.Location = new System.Drawing.Point(12, 138);
+            this.lblArchivoOriginal.Name = "lblArchivoOriginal";
+            this.lblArchivoOriginal.Size = new System.Drawing.Size(750, 76);
+            this.lblArchivoOriginal.TabIndex = 3;
+            this.lblArchivoOriginal.Text = "Archivo Original";
+            this.lblArchivoOriginal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblArchivoDescontar
+            // 
+            this.lblArchivoDescontar.Location = new System.Drawing.Point(12, 289);
+            this.lblArchivoDescontar.Name = "lblArchivoDescontar";
+            this.lblArchivoDescontar.Size = new System.Drawing.Size(750, 76);
+            this.lblArchivoDescontar.TabIndex = 4;
+            this.lblArchivoDescontar.Text = "Archivo Descontar";
+            this.lblArchivoDescontar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 529);
+            this.Controls.Add(this.lblArchivoDescontar);
+            this.Controls.Add(this.lblArchivoOriginal);
+            this.Controls.Add(this.BtnSaveResult);
+            this.Controls.Add(this.BtnArchivoRestar);
+            this.Controls.Add(this.BtnArchivoOriginal);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Restar Archivos de texto lineas repetidas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnArchivoOriginal;
+        private System.Windows.Forms.Button BtnArchivoRestar;
+        private System.Windows.Forms.Button BtnSaveResult;
+        private System.Windows.Forms.Label lblArchivoOriginal;
+        private System.Windows.Forms.Label lblArchivoDescontar;
+    }
+}
+
---
+++ RestarArchivos/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace RestarArchivos
+{
+    public partial class Form1 : Form
+    {
+        private List<string> mainFileLines;
+        private List<string> filterFileLines;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lblArchivoOriginal.Text = string.Empty;
+            lblArchivoDescontar.Text = string.Empty;
+        }
+
+        private void BtnArchivoOriginal_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    mainFileLines = File.ReadAllLines(openFileDialog.FileName)
+                                        .Select(line => line.Trim())
+                                        .ToList();
+                    lblArchivoOriginal.Text = openFileDialog.FileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al leer el archivo Original: {ex.Message}");
+            }
+        }
+    
+        private void BtnArchivoRestar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filterFileLines = File.ReadAllLines(openFileDialog.FileName)
+                                         .Select(line => line.Trim())
+                                         .ToList();
+                    lblArchivoDescontar .Text = openFileDialog.FileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al leer el archivo a Descontar: {ex.Message}");
+            }
+            
+        }
+
+        private void BtnSaveResult_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                if (mainFileLines != null && filterFileLines != null)
+                {
+                    var resultLines = mainFileLines.Except(filterFileLines).ToList();
+                    SaveFileDialog saveFileDialog = new SaveFileDialog();
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        File.WriteAllLines(saveFileDialog.FileName, resultLines);
+                        MessageBox.Show($"Se guardo el archivo {saveFileDialog.FileName}");
+                    }
+                    ReinicializarFormulario();
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, selecciona ambos archivos primero.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al guardar el archivo: {ex.Message}");
+            }
+        }
+        private void ReinicializarFormulario()
+        {
+            // Restablecer controles y variables
+            mainFileLines = null;
+            filterFileLines = null;
+            lblArchivoOriginal.Text = string.Empty;
+            lblArchivoDescontar.Text = string.Empty;
+        }
+    }
+}
